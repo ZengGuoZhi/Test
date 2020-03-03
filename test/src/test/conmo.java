ddf13df6d4e1a77131212fc3c10233f9822c330e
曾国智 <1024511729@qq.com>
2020-03-04 00:30:48 +0800

    第二次提交

--- test/src/test/conmo.java
+++ test/src/test/conmo.java
@@ -3,6 +3,7 @@ package test;
 public class conmo {
     public static void main(String[] args) {
         System.out.println("hello");
+		 System.out.println("hello");
         System.out.println();
     }
 }
