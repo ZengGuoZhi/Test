951837056189f781c8785465b587d819a2ba1833
曾国智 <1024511729@qq.com>
2020-03-04 00:33:49 +0800

    123

--- test/src/test/conmo.java
+++ test/src/test/conmo.java
@@ -3,7 +3,8 @@ package test;
 public class conmo {
     public static void main(String[] args) {
         System.out.println("hello");
-		 System.out.println("hello");
         System.out.println();
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
